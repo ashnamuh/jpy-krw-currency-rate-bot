8b4fa05da88b92d99b2dbbac90d746962c12368e
Jae Mok <ashnamuh@gmail.com>
2018-08-30 17:23:16 +0900

    enhance bot message body

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const schedule = require('node-schedule');
 const config = require('config');
+const moment = require('moment');
+const schedule = require('node-schedule');
 const {getJpyCurrencyRate} = require('./lib/currency');
 const telegramBot = require('./lib/telegram-bot');
 
-schedule.scheduleJob('*/1 * * * *', () => {
+schedule.scheduleJob('0 */1 * * *', () => {
+	const now = moment().format('l LT');
+
 	getJpyCurrencyRate(100)
-		.then(rate => telegramBot.sendMessage(config.telegramBot.chatId, rate.toFixed(3)));
+		.then(rate => telegramBot.sendMessage(config.telegramBot.chatId, `${now} - 100JYP --> ${rate.toFixed(3)}KRW.`));
 });
 
 telegramBot.onText(/\/rate/, msg => {
 	const chatId = msg.chat.id;
 
 	getJpyCurrencyRate(100)
-		.then(rate => telegramBot.sendMessage(chatId, rate.toFixed(3)));
+		.then(rate => telegramBot.sendMessage(chatId, `100JYP is ${rate.toFixed(3)}KRW now.`));
 });
