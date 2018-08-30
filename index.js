040577d9a397a442c130dd30cb869a2e957e2aae
Jae Mok <ashnamuh@gmail.com>
2018-08-30 16:58:55 +0900

    add polling rate message

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ schedule.scheduleJob('*/1 * * * *', () => {
 	getJpyCurrencyRate(100)
 		.then(rate => telegramBot.sendMessage(config.telegramBot.chatId, rate.toFixed(3)));
 });
+
+telegramBot.onText(/\/rate/, msg => {
+	const chatId = msg.chat.id;
+
+	getJpyCurrencyRate(100)
+		.then(rate => telegramBot.sendMessage(chatId, rate.toFixed(3)));
+});
